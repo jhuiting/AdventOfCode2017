0a64a05fc98e2075acb2d213af01a32b1b48f828
Jos Huiting <jos.huiting@mendix.com>
2017-12-04 09:09:42 +0100

    Refactored Day 4 solution a bit

--- src/day4/Day4.kt
+++ src/day4/Day4.kt
@@ -5,22 +5,18 @@ import util.InputUtils
 class PasswordPolicySolver {
     companion object {
         fun solvePart1(input: List<List<String>>) {
-            val uniquePasswordsCount = input.filter { it.distinct().count() == it.size }.count()
-            println("Solution password duplication part 1: $uniquePasswordsCount")
+            println("Solution password duplication part 1: ${input.filter { it.distinct().count() == it.size }.count()}")
         }
 
         fun solvePart2(input: List<List<String>>) {
             val sortedPasswords = input.map { it.map{ it.toSortedSet() }  }
-            val uniquePasswordsCount = sortedPasswords.filter { it.distinct().count() == it.size }.count()
-            println("Solution password duplication part 2: $uniquePasswordsCount")
+            println("Solution password duplication part 2: ${sortedPasswords.filter { it.distinct().count() == it.size }.count()}")
         }
     }
 }
 
-
 fun main(args : Array<String>) {
-    val inputLists = InputUtils.getInputAsLineArray("day4")
-            .map{ it.split(" ") }
+    val inputLists = InputUtils.getInputAsLineArray("day4").map{ it.split(" ") }
 
     PasswordPolicySolver.solvePart1(inputLists)
     PasswordPolicySolver.solvePart2(inputLists)
